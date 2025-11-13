85712fce06bc119ce21943cf31e76a722aaa4ec8
Matteo Valenzi <dilloawallace@hotmail.com>
2025-11-13 11:18:10 +0100

    initial

---
+++ Models/Persona.cs
@@ -0,0 +1,6 @@
+namespace cs_oop.Models;
+
+class Persona
+{
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using cs_oop.Models;
+
+namespace cs_oop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona gigi = new Persona();
+        var mario = new Persona();
+        Persona anna = new();
+    }
+}
+
+
+
+
+
